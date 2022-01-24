2f5a1619c3d00acc7c12d662c2fa774bc3fe05d2
isultanov99 <isultanov99@gmail.com>
2022-01-24 12:29:25 +0300

    fixing for true asynchrony, adding redirect to get-data page

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import asyncio
 
 import aiohttp
-from flask import Flask, json
+from flask import Flask, json, redirect, url_for
 
-URL1 = 'http://localhost:5000/static/data1.json'
-URL2 = 'http://localhost:5000/static/data2.json'
-URL3 = 'http://localhost:5000/static/data3.json'
-# URL3 = 'http://www.google.com:81/'                              # URL with timeout for test
+URLS = [
+    'http://localhost:5000/static/data1.json',
+    'http://localhost:5000/static/data2.json',
+    'http://localhost:5000/static/data3.json',
+]
+# 'http://www.google.com:81/'                              # URL with timeout for test
 
 
 async def getjson(session, url) -> list:
     response_json = []
     try:
-        response = await session.get(url)
-        response_json = await response.json()
+        async with session.get(url) as resp:
+            response_json = await resp.json()
     except asyncio.TimeoutError:
         print('A timeout occurred.')
         pass
@@ -24,13 +26,14 @@ async def getjson(session, url) -> list:
 
 
 async def aiohttp_main():
-    data = []
     timeout = aiohttp.ClientTimeout(total=2)    # Overriding default timeout with 2 seconds
     async with aiohttp.ClientSession(timeout=timeout) as session:
-        data.extend(await getjson(session, URL1))
-        data.extend(await getjson(session, URL2))
-        data.extend(await getjson(session, URL3))
-    return data
+        data = []
+        for url in URLS:
+            data.append(asyncio.ensure_future(getjson(session, url)))
+
+        all_data = await asyncio.gather(*data)
+    return all_data
 
 
 app = Flask(__name__,  static_url_path='/static')
@@ -40,9 +43,13 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 @app.route('/get-data')
 def get_data():
     data = asyncio.run(aiohttp_main())
+    data = [item for sublist in data for item in sublist]
     data = sorted(data, key=lambda d: d['id'])
     return json.jsonify(data)
 
+@app.route('/')
+def home():
+    return redirect(url_for('get_data'))
 
 if __name__ == '__main__':
     app.run()
